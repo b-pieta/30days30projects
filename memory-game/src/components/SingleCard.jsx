0a60b4ff1e491dbb00019765871b134c1b44e66d
b-pieta <bozena.pieta95@gmail.com>
2024-03-02 00:52:42 +0100

    Mobile version v1

--- memory-game/src/components/SingleCard.jsx
+++ memory-game/src/components/SingleCard.jsx
@@ -14,7 +14,7 @@ function SingleCard({ card, handleChoice, flipped }) {
     <div className={cardClasses} onClick={handleClick}>
       <div className="card-inner">
         <div className='front'>
-        <FontAwesomeIcon icon={card.src} size="9x" style={{ color: '#a393eb' }} />
+        <FontAwesomeIcon className="icon-large" icon={card.src} size="9x" style={{ color: '#a393eb' }} />
         </div>
         <div className='back'>
           <img src={back} alt='card back' />
