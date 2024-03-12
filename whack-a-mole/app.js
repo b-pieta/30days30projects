9e6081afd6eeb13c0f3edbdf8a52be92a958d76c
b-pieta <bozena.pieta95@gmail.com>
2024-03-13 00:59:00 +0100

    Finished

--- whack-a-mole/app.js
+++ whack-a-mole/app.js
@@ -1,17 +1,51 @@
-const squares = document.querySelectorAll('.square')
-const mole = document.querySelector('.mole')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const squares = document.querySelectorAll('.square');
+const timeLeft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
 
+let result = 0;
+let hitPosition;
+let currentTime = 60;
+let timerId = null; 
+let countDownTimerId = null; 
 
-//removing a mole from our grid
+// Removing a mole from our grid
 function randomSquare() {
-    squares.forEach(square => {
-        square.classList.remove('mole')
-    })
+    squares.forEach((square) => {
+        square.classList.remove('mole');
+    });
 
-    let randomPosition = squares[Math.floor(Math.random() * 9)]
-    console.log(randomPosition)
+    let randomPosition = squares[Math.floor(Math.random() * squares.length)];
+    randomPosition.classList.add('mole');
+
+    hitPosition = randomPosition.id;
 }
 
-randomSquare()
+// Setting a timer to catch a mole
+squares.forEach((square) => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    });
+});
+
+function countDown() {
+    currentTime--;
+    timeLeft.textContent = currentTime;
+
+    if (currentTime === 0) {
+        clearInterval(timerId); 
+        clearInterval(countDownTimerId); 
+        alert('Game over! Your final score is ' + result);
+    }
+}
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500); 
+}
+
+
+moveMole();
+countDownTimerId = setInterval(countDown, 1000); 
